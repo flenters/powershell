52efe9876f9c7c43c08f43cfcefc08b48801a2e0
flenters <flenters@gmail.com>
2018-11-19 11:19:49 +0200

    Add files via upload

---
+++ get-user-data.ps1
@@ -0,0 +1,5 @@
+﻿#This will list all the AD users groups
+#Read user name
+$member = Read-Host "Please type in the users mnemonic"
+$user = get-aduser -Identity $member -Properties MemberOf | select MemberOf
+	(($user.memberof).split(",") | where-object {$_.contains("CN=")}).replace("CN=","")
