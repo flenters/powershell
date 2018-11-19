7c432ad7b338113aa4dff53025b82853389c5972
flenters <flenters@gmail.com>
2018-11-19 11:22:06 +0200

    Update get-user-data.ps1

--- get-user-data.ps1
+++ get-user-data.ps1
@@ -1,5 +1,5 @@
 ﻿#This will list all the AD users groups
 #Read user name
-$member = Read-Host "Please type in the users mnemonic"
+$member = Read-Host "Please type in the AD username"
 $user = get-aduser -Identity $member -Properties MemberOf | select MemberOf
 	(($user.memberof).split(",") | where-object {$_.contains("CN=")}).replace("CN=","")
