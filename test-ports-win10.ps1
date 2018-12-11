c38ed448be72a3c89f0ec9417bf449888d92deaf
flenters <flenters@gmail.com>
2018-12-11 11:15:16 +0200

    Add files via upload

---
+++ test-ports-win10.ps1
@@ -0,0 +1,16 @@
+﻿#Type in the hostname you would like to check
+$computerName = @("www.google.co.za","www.github.com")
+#type in the ports you want to check
+$ports = @("80","443")
+
+foreach ($computer in $computerName){
+    foreach($port in $ports){
+        $test = Test-NetConnection -ComputerName "$computer" -Port "$port" -InformationLevel Quiet -WarningAction SilentlyContinue
+    if($test -eq $true){
+            Write-Host "$computer" Port "$port" is open -ForegroundColor Green
+        }
+        else{
+            Write-Host "$computer" Port "$port" is close -ForegroundColor Red
+        }
+       }
+      }
