772d78d90322fd6f3f1065031e24a00c00c2d009
flenters <flenters@gmail.com>
2019-01-07 09:25:14 +0200

    Update basic-ssh.ps1

--- basic-ssh.ps1
+++ basic-ssh.ps1
@@ -3,7 +3,7 @@ $pass = ConvertTo-SecureString -String "password" -AsPlainText -Force
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "$user", $pass
 Start-Sleep -s 2
 #Create new SSH Session
-New-SSHSession -ComputerName "rmb-vpr-dvprodjb" -Credential $cred
+New-SSHSession -ComputerName "hostname" -Credential $cred
 Start-Sleep -s 2
 #Run remote SSH command
 Invoke-SSHCommand -Index 0 -Command 'ls -l ~/'
