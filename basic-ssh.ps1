8f8d4104268d36d2cf1b6addd651a5a776d03fae
flenters <flenters@gmail.com>
2019-01-07 09:24:01 +0200

    Add files via upload

---
+++ basic-ssh.ps1
@@ -0,0 +1,11 @@
+﻿$user = "username"
+$pass = ConvertTo-SecureString -String "password" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "$user", $pass
+Start-Sleep -s 2
+#Create new SSH Session
+New-SSHSession -ComputerName "rmb-vpr-dvprodjb" -Credential $cred
+Start-Sleep -s 2
+#Run remote SSH command
+Invoke-SSHCommand -Index 0 -Command 'ls -l ~/'
+#Remove the ssh session
+Remove-SSHSession -Index 0 -Verbose
