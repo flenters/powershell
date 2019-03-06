894721b3f5ddc5be44cef7b6ded2dc631b3cde40
flenters <flenters@gmail.com>
2019-03-06 13:36:34 +0200

    Add files via upload

---
+++ search-string.ps1
@@ -0,0 +1,4 @@
+﻿$string = Read-Host "Type the string you are searching for"
+$path = Read-host "Type in the full path to the folder e.g c:\temp"
+
+Get-ChildItem -Path $path -Recurse | Select-String "$string"
